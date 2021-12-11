169ed1c3fabf37edff59acd54a735ee608a19721
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2021-12-11 19:20:28 +0900

    Fix classes in internal.front.validator

--- src/main/java/silverchain/internal/front/validator/InvalidRepetitionValidator.java
+++ src/main/java/silverchain/internal/front/validator/InvalidRepetitionValidator.java
@@ -1,5 +1,7 @@
 package silverchain.internal.front.validator;
 
+import static java.lang.Integer.parseInt;
+
 import org.antlr.v4.runtime.Token;
 import silverchain.internal.front.parser.antlr.AgBaseListener;
 import silverchain.internal.front.parser.antlr.AgParser.RepeatSugarContext;
@@ -10,10 +12,8 @@ public class InvalidRepetitionValidator extends AgBaseListener {
   public void enterRepeatSugar(RepeatSugarContext ctx) {
     Token min = ctx.MIN;
     Token max = ctx.MAX;
-    if (min != null && max != null) {
-      if (Integer.parseInt(min.getText()) > Integer.parseInt(max.getText())) {
-        throw new InvalidRepetition(ctx);
-      }
+    if (min != null && max != null && parseInt(min.getText()) > parseInt(max.getText())) {
+      throw new InvalidRepetition(ctx);
     }
   }
 }
