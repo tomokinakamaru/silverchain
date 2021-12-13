4f4e34d51254efe34725e2020c70b896ccc1c625
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2021-12-13 14:08:48 +0900

    Update AntlrParserTests

--- src/test/java/internal/frontend/AntlrParserTests.java
+++ src/test/java/internal/frontend/AntlrParserTests.java
@@ -17,82 +17,222 @@ import silverchain.internal.frontend.parser.antlr.AgParser;
 
 class AntlrParserTests {
 
-  private static final ErrorListener ERROR_LISTENER = new ErrorListener();
-
   private static Arguments[] testData() {
-    return new Arguments[] {
-      args(AgParser::input, ""),
-      args(AgParser::importDecl, "import foo;"),
-      args(AgParser::fragmentDecl, "$FOO = foo();"),
-      args(AgParser::typeDecl, "Foo { Foo foo(); }"),
-      args(AgParser::typeDecl, "Foo<T> { Foo foo(); }"),
-      args(AgParser::typeDecl, "Foo<T;S> { Foo foo(); }"),
-      args(AgParser::typeDecl, "Foo<;T> { Foo foo(); }"),
-      args(AgParser::chainStmts, "Foo foo(); Foo foo();"),
-      args(AgParser::chainStmt, "Foo foo();"),
-      args(AgParser::chainExpr, "foo() | foo()"),
-      args(AgParser::chainTerm, "foo() foo()"),
-      args(AgParser::chainFact, "foo()*"),
-      args(AgParser::returnType, "void"),
-      args(AgParser::repeat, "*"),
-      args(AgParser::repeat, "?"),
-      args(AgParser::repeat, "+"),
-      args(AgParser::repeatSugar, "[1]"),
-      args(AgParser::repeatSugar, "[1,]"),
-      args(AgParser::repeatSugar, "[1,2]"),
-      args(AgParser::permutation, "{ foo() }"),
-      args(AgParser::permutation, "{ foo(), }"),
-      args(AgParser::permutation, "{ foo(), foo() }"),
-      args(AgParser::permutation, "{ foo(), foo(), }"),
-      args(AgParser::method, "foo<T>()"),
-      args(AgParser::method, "foo(Foo foo)"),
-      args(AgParser::method, "foo() throws Foo"),
-      args(AgParser::exceptions, "throws Foo, Foo"),
-      args(AgParser::params, "Foo foo, Foo foo"),
-      args(AgParser::param, "Foo... foo"),
-      args(AgParser::fragmentRef, "$FOO"),
-      args(AgParser::typeRef, "Foo<Foo>"),
-      args(AgParser::typeRef, "Foo[]"),
-      args(AgParser::typeRef, "Foo[][]"),
-      args(AgParser::typeArgs, "<T,S>"),
-      args(AgParser::typeArg, "Foo"),
-      args(AgParser::typeArg, "?"),
-      args(AgParser::wildcard, "? super Foo"),
-      args(AgParser::wildcard, "? extends Foo"),
-      args(AgParser::typeParams, "T, S"),
-      args(AgParser::typeParam, "T extends Foo"),
-      args(AgParser::bounds, "extends Foo & Foo"),
-      args(AgParser::name, "foo"),
-      args(AgParser::name, "foo.foo"),
-      args(AgParser::name, "foo.foo.foo"),
+    // @formatter:off
+    return new Arguments[]{
+        args(
+            AgParser::input,
+            "",
+            "(input <EOF>)"),
+        args(
+            AgParser::importDecl,
+            "import foo;",
+            "(importDecl import (name foo) ;)"),
+        args(
+            AgParser::fragmentDecl,
+            "$FOO = foo();",
+            "(fragmentDecl $FOO = (chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) ;)"),
+        args(
+            AgParser::typeDecl,
+            "Foo { Foo foo(); }",
+            "(typeDecl (name Foo) { (chainStmts (chainStmt (returnType (typeRef (name Foo))) " +
+                "(chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) ;)) })"),
+        args(
+            AgParser::typeDecl,
+            "Foo<T> { Foo foo(); }",
+            "(typeDecl (name Foo) < (typeParams (typeParam T)) > { (chainStmts (chainStmt (returnType (typeRef (name Foo))) " +
+                "(chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) ;)) })"),
+        args(
+            AgParser::typeDecl,
+            "Foo<T;S> { Foo foo(); }",
+            "(typeDecl (name Foo) < (typeParams (typeParam T)) ; (typeParams (typeParam S)) > { (chainStmts (chainStmt " +
+                "(returnType (typeRef (name Foo))) (chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) ;)) })"),
+        args(
+            AgParser::typeDecl,
+            "Foo<;T> { Foo foo(); }",
+            "(typeDecl (name Foo) < ; (typeParams (typeParam T)) > { (chainStmts (chainStmt " +
+                "(returnType (typeRef (name Foo))) (chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) ;)) })"),
+        args(
+            AgParser::chainStmts,
+            "Foo foo(); Foo foo();",
+            "(chainStmts " +
+                "(chainStmt (returnType (typeRef (name Foo))) (chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) ;) " +
+                "(chainStmt (returnType (typeRef (name Foo))) (chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) ;))"),
+        args(
+            AgParser::chainStmt,
+            "Foo foo();",
+            "(chainStmt (returnType (typeRef (name Foo))) (chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) ;)"),
+        args(
+            AgParser::chainExpr,
+            "foo() | foo()",
+            "(chainExpr (chainTerm (chainFact (chainElem (method foo ( ))))) | (chainTerm (chainFact (chainElem (method foo ( ))))))"),
+        args(
+            AgParser::chainTerm,
+            "foo() foo()",
+            "(chainTerm (chainFact (chainElem (method foo ( )))) (chainFact (chainElem (method foo ( )))))"),
+        args(
+            AgParser::chainFact,
+            "foo()*",
+            "(chainFact (chainElem (method foo ( ))) (repeat *))"),
+        args(
+            AgParser::returnType,
+            "Foo",
+            "(returnType (typeRef (name Foo)))"),
+        args(
+            AgParser::repeat,
+            "*",
+            "(repeat *)"),
+        args(
+            AgParser::repeat,
+            "?",
+            "(repeat ?)"),
+        args(
+            AgParser::repeat,
+            "+",
+            "(repeat +)"),
+        args(
+            AgParser::repeatSugar,
+            "[1]",
+            "(repeatSugar [ 1 ])"),
+        args(
+            AgParser::repeatSugar,
+            "[1,]",
+            "(repeatSugar [ 1 , ])"),
+        args(
+            AgParser::repeatSugar,
+            "[1,2]",
+            "(repeatSugar [ 1 , 2 ])"),
+        args(
+            AgParser::permutation,
+            "{ foo() }",
+            "(permutation { (chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) })"),
+        args(
+            AgParser::permutation,
+            "{ foo(), }",
+            "(permutation { (chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) , })"),
+        args(
+            AgParser::permutation,
+            "{ foo(), foo() }",
+            "(permutation { " +
+                "(chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) , " +
+                "(chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) })"),
+        args(
+            AgParser::permutation,
+            "{ foo(), foo(), }",
+            "(permutation { " +
+                "(chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) , " +
+                "(chainExpr (chainTerm (chainFact (chainElem (method foo ( )))))) , })"),
+        args(
+            AgParser::method,
+            "foo<T>()",
+            "(method foo < (typeParams (typeParam T)) > ( ))"),
+        args(
+            AgParser::method,
+            "foo(Foo foo)",
+            "(method foo ( (params (param (typeRef (name Foo)) foo)) ))"),
+        args(
+            AgParser::method,
+            "foo() throws Foo",
+            "(method foo ( ) (exceptions throws (typeRef (name Foo))))"),
+        args(
+            AgParser::exceptions,
+            "throws Foo, Foo",
+            "(exceptions throws (typeRef (name Foo)) , (typeRef (name Foo)))"),
+        args(
+            AgParser::params,
+            "Foo foo, Foo foo",
+            "(params (param (typeRef (name Foo)) foo) , (param (typeRef (name Foo)) foo))"),
+        args(
+            AgParser::param,
+            "Foo... foo",
+            "(param (typeRef (name Foo)) ... foo)"),
+        args(
+            AgParser::fragmentRef,
+            "$FOO",
+            "(fragmentRef $FOO)"),
+        args(
+            AgParser::typeRef,
+            "Foo<Foo>",
+            "(typeRef (name Foo) (typeArgs < (typeArg (typeRef (name Foo))) >))"),
+        args(
+            AgParser::typeRef,
+            "Foo[]",
+            "(typeRef (name Foo) [])"),
+        args(
+            AgParser::typeRef,
+            "Foo[][]",
+            "(typeRef (name Foo) [] [])"),
+        args(
+            AgParser::typeArgs,
+            "<T,S>",
+            "(typeArgs < (typeArg (typeRef (name T))) , (typeArg (typeRef (name S))) >)"),
+        args(
+            AgParser::typeArg,
+            "Foo",
+            "(typeArg (typeRef (name Foo)))"),
+        args(
+            AgParser::typeArg,
+            "?",
+            "(typeArg (wildcard ?))"),
+        args(
+            AgParser::wildcard,
+            "? super Foo",
+            "(wildcard ? super (typeRef (name Foo)))"),
+        args(
+            AgParser::wildcard,
+            "? extends Foo",
+            "(wildcard ? extends (typeRef (name Foo)))"),
+        args(
+            AgParser::typeParams,
+            "T, S",
+            "(typeParams (typeParam T) , (typeParam S))"),
+        args(
+            AgParser::typeParam,
+            "T extends Foo",
+            "(typeParam T (bounds extends (typeRef (name Foo))))"),
+        args(
+            AgParser::bounds,
+            "extends Foo & Foo",
+            "(bounds extends (typeRef (name Foo)) & (typeRef (name Foo)))"),
+        args(
+            AgParser::name,
+            "foo",
+            "(name foo)"),
+        args(
+            AgParser::name,
+            "foo.foo",
+            "(name (name foo) . foo)"),
+        args(
+            AgParser::name,
+            "foo.foo.foo",
+            "(name (name (name foo) . foo) . foo)"),
     };
+    // @formatter:on
   }
 
-  @ParameterizedTest(name = "[{index}] \"{1}\"")
+  @ParameterizedTest(name = "[{index}] \"{1}\" ->\"{2}\"")
   @MethodSource("testData")
-  void test(Function<AgParser, ParserRuleContext> selector, String text) {
+  void test(Function<AgParser, ParserRuleContext> selector, String text, String expected) {
     AgLexer lexer = new AgLexer(CharStreams.fromString(text));
     AgParser parser = new AgParser(new CommonTokenStream(lexer));
     parser.removeErrorListeners();
     parser.addErrorListener(ERROR_LISTENER);
     ParserRuleContext ctx = selector.apply(parser);
-    assertThat(ctx).isNotNull();
+    assertThat(ctx.toStringTree(parser)).isEqualTo(expected);
   }
 
-  private static Arguments args(Function<AgParser, ParserRuleContext> selector, String text) {
-    return Arguments.of(selector, text);
+  private static final ErrorListener ERROR_LISTENER = new ErrorListener();
+
+  private static Arguments args(
+      Function<AgParser, ParserRuleContext> selector, String text, String expected) {
+    return Arguments.of(selector, text, expected);
   }
 
   private static class ErrorListener extends BaseErrorListener {
     @Override
     public void syntaxError(
-        Recognizer<?, ?> recognizer,
-        Object offendingSymbol,
-        int line,
-        int charPositionInLine,
-        String msg,
-        RecognitionException e) {
-      throw new RuntimeException("L" + line + "C" + charPositionInLine + ":" + msg);
+        Recognizer r, Object o, int l, int c, String m, RecognitionException e) {
+      throw new RuntimeException(String.format("L%dC%d: %s", l, c, m));
     }
   }
 }
