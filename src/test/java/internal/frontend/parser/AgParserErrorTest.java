cf60035745346358d82211877c95bbecfab74cc3
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2021-12-15 15:19:39 +0900

    Refactor internal.frontend.parser.*

--- src/test/java/internal/frontend/parser/AgParserErrorTest.java
+++ src/test/java/internal/frontend/parser/AgParserErrorTest.java
@@ -1,39 +1,30 @@
-package internal.frontend;
+package internal.frontend.parser;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import silverchain.internal.frontend.parser.AgParser;
 import silverchain.internal.frontend.parser.SyntaxError;
-import silverchain.internal.frontend.parser.antlr.AgParser.InputContext;
 
-class AgParserTests {
+class AgParserErrorTest {
 
-  private static Arguments[] testSyntaxErrorData() {
+  private static Arguments[] data() {
     return new Arguments[] {
       Arguments.of("!", "Syntax error: token recognition error at: '!' (L1C1)")
     };
   }
 
   @ParameterizedTest(name = "[{index}] \"{0}\" -> \"{1}\"")
-  @MethodSource("testSyntaxErrorData")
-  void testSyntaxError(String text, String message) {
+  @MethodSource("data")
+  void test(String text, String message) {
     AgParser parser = new AgParser();
     CharStream stream = CharStreams.fromString(text);
     assertThatThrownBy(() -> parser.parse(stream))
         .isExactlyInstanceOf(SyntaxError.class)
         .hasMessage(message);
-  }
-
-  @Test
-  void testNoError() {
-    InputContext ctx = new AgParser().parse(CharStreams.fromString(""));
-    assertThat(ctx).isNotNull();
   }
 }
--- src/test/java/internal/frontend/parser/AntlrParserTests.java
+++ src/test/java/internal/frontend/parser/AntlrParserTests.java
@@ -1,14 +1,11 @@
-package internal.frontend;
+package internal.frontend.parser;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.function.Function;
-import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,7 +14,7 @@ import silverchain.internal.frontend.parser.antlr.AgParser;
 
 class AntlrParserTests {
 
-  private static Arguments[] testData() {
+  private static Arguments[] data() {
     // @formatter:off
     return new Arguments[]{
         args(
@@ -73,23 +70,19 @@ class AntlrParserTests {
         args(
             AgParser::chainFact,
             "foo()*",
-            "(chainFact (chainElem (method foo ( ))) (repeatSugar *))"),
+            "(chainFact (chainElem (method foo ( ))) *)"),
+        args(
+            AgParser::chainFact,
+            "foo()+",
+            "(chainFact (chainElem (method foo ( ))) +)"),
+        args(
+            AgParser::chainFact,
+            "foo()?",
+            "(chainFact (chainElem (method foo ( ))) ?)"),
         args(
             AgParser::returnType,
             "Foo",
             "(returnType (typeRef (name Foo)))"),
-        args(
-            AgParser::repeatSugar,
-            "*",
-            "(repeatSugar *)"),
-        args(
-            AgParser::repeatSugar,
-            "?",
-            "(repeatSugar ?)"),
-        args(
-            AgParser::repeatSugar,
-            "+",
-            "(repeatSugar +)"),
         args(
             AgParser::repeat,
             "[1]",
@@ -211,28 +204,17 @@ class AntlrParserTests {
   }
 
   @ParameterizedTest(name = "[{index}] \"{1}\" ->\"{2}\"")
-  @MethodSource("testData")
+  @MethodSource("data")
   void test(Function<AgParser, ParserRuleContext> selector, String text, String expected) {
     AgLexer lexer = new AgLexer(CharStreams.fromString(text));
     AgParser parser = new AgParser(new CommonTokenStream(lexer));
-    parser.removeErrorListeners();
-    parser.addErrorListener(ERROR_LISTENER);
     ParserRuleContext ctx = selector.apply(parser);
     assertThat(ctx.toStringTree(parser)).isEqualTo(expected);
   }
 
-  private static final ErrorListener ERROR_LISTENER = new ErrorListener();
-
-  private static Arguments args(
-      Function<AgParser, ParserRuleContext> selector, String text, String expected) {
+  private static Arguments args(Selector selector, String text, String expected) {
     return Arguments.of(selector, text, expected);
   }
 
-  private static class ErrorListener extends BaseErrorListener {
-    @Override
-    public void syntaxError(
-        Recognizer r, Object o, int l, int c, String m, RecognitionException e) {
-      throw new RuntimeException(String.format("L%dC%d: %s", l, c, m));
-    }
-  }
+  private interface Selector extends Function<AgParser, ParserRuleContext> {}
 }
