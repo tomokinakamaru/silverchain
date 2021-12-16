4cf03c79e5046138fc635cf15919a0f2e10d77b5
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2021-12-17 00:17:41 +0900

    Import context classes

--- src/main/java/silverchain/internal/frontend/rewriter/utility/ContextBuilder.java
+++ src/main/java/silverchain/internal/frontend/rewriter/utility/ContextBuilder.java
@@ -7,145 +7,171 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import org.apiguardian.api.API;
-import silverchain.internal.frontend.parser.antlr.AgParser;
+import silverchain.internal.frontend.parser.antlr.AgParser.BoundsContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ChainElemContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ChainExprContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ChainFactContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ChainStmtContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ChainStmtsContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ChainTermContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ExceptionsContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.FragmentDeclContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.FragmentRefContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ImportDeclContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.InputContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.MethodContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.NameContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ParamContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ParamsContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.PermutationContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.QualifierContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.RepeatContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.ReturnTypeContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.TypeArgContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.TypeArgsContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.TypeDeclContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.TypeParamContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.TypeParamsContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.TypeRefContext;
+import silverchain.internal.frontend.parser.antlr.AgParser.WildcardContext;
 import silverchain.internal.frontend.parser.antlr.AgVisitor;
 
 @API(status = API.Status.INTERNAL)
 public abstract class ContextBuilder implements AgVisitor<ParseTree> {
 
   @Override
-  public ParseTree visitInput(AgParser.InputContext ctx) {
-    return build(ctx, AgParser.InputContext::new);
+  public ParseTree visitInput(InputContext ctx) {
+    return build(ctx, InputContext::new);
   }
 
   @Override
-  public ParseTree visitImportDecl(AgParser.ImportDeclContext ctx) {
-    return build(ctx, AgParser.ImportDeclContext::new);
+  public ParseTree visitImportDecl(ImportDeclContext ctx) {
+    return build(ctx, ImportDeclContext::new);
   }
 
   @Override
-  public ParseTree visitFragmentDecl(AgParser.FragmentDeclContext ctx) {
-    return build(ctx, AgParser.FragmentDeclContext::new);
+  public ParseTree visitFragmentDecl(FragmentDeclContext ctx) {
+    return build(ctx, FragmentDeclContext::new);
   }
 
   @Override
-  public ParseTree visitTypeDecl(AgParser.TypeDeclContext ctx) {
-    return build(ctx, AgParser.TypeDeclContext::new);
+  public ParseTree visitTypeDecl(TypeDeclContext ctx) {
+    return build(ctx, TypeDeclContext::new);
   }
 
   @Override
-  public ParseTree visitChainStmts(AgParser.ChainStmtsContext ctx) {
-    return build(ctx, AgParser.ChainStmtsContext::new);
+  public ParseTree visitChainStmts(ChainStmtsContext ctx) {
+    return build(ctx, ChainStmtsContext::new);
   }
 
   @Override
-  public ParseTree visitChainStmt(AgParser.ChainStmtContext ctx) {
-    return build(ctx, AgParser.ChainStmtContext::new);
+  public ParseTree visitChainStmt(ChainStmtContext ctx) {
+    return build(ctx, ChainStmtContext::new);
   }
 
   @Override
-  public ParseTree visitChainExpr(AgParser.ChainExprContext ctx) {
-    return build(ctx, AgParser.ChainExprContext::new);
+  public ParseTree visitChainExpr(ChainExprContext ctx) {
+    return build(ctx, ChainExprContext::new);
   }
 
   @Override
-  public ParseTree visitChainTerm(AgParser.ChainTermContext ctx) {
-    return build(ctx, AgParser.ChainTermContext::new);
+  public ParseTree visitChainTerm(ChainTermContext ctx) {
+    return build(ctx, ChainTermContext::new);
   }
 
   @Override
-  public ParseTree visitChainFact(AgParser.ChainFactContext ctx) {
-    return build(ctx, AgParser.ChainFactContext::new);
+  public ParseTree visitChainFact(ChainFactContext ctx) {
+    return build(ctx, ChainFactContext::new);
   }
 
   @Override
-  public ParseTree visitChainElem(AgParser.ChainElemContext ctx) {
-    return build(ctx, AgParser.ChainElemContext::new);
+  public ParseTree visitChainElem(ChainElemContext ctx) {
+    return build(ctx, ChainElemContext::new);
   }
 
   @Override
-  public ParseTree visitReturnType(AgParser.ReturnTypeContext ctx) {
-    return build(ctx, AgParser.ReturnTypeContext::new);
+  public ParseTree visitReturnType(ReturnTypeContext ctx) {
+    return build(ctx, ReturnTypeContext::new);
   }
 
   @Override
-  public ParseTree visitRepeat(AgParser.RepeatContext ctx) {
-    return build(ctx, AgParser.RepeatContext::new);
+  public ParseTree visitRepeat(RepeatContext ctx) {
+    return build(ctx, RepeatContext::new);
   }
 
   @Override
-  public ParseTree visitPermutation(AgParser.PermutationContext ctx) {
-    return build(ctx, AgParser.PermutationContext::new);
+  public ParseTree visitPermutation(PermutationContext ctx) {
+    return build(ctx, PermutationContext::new);
   }
 
   @Override
-  public ParseTree visitMethod(AgParser.MethodContext ctx) {
-    return build(ctx, AgParser.MethodContext::new);
+  public ParseTree visitMethod(MethodContext ctx) {
+    return build(ctx, MethodContext::new);
   }
 
   @Override
-  public ParseTree visitExceptions(AgParser.ExceptionsContext ctx) {
-    return build(ctx, AgParser.ExceptionsContext::new);
+  public ParseTree visitExceptions(ExceptionsContext ctx) {
+    return build(ctx, ExceptionsContext::new);
   }
 
   @Override
-  public ParseTree visitParams(AgParser.ParamsContext ctx) {
-    return build(ctx, AgParser.ParamsContext::new);
+  public ParseTree visitParams(ParamsContext ctx) {
+    return build(ctx, ParamsContext::new);
   }
 
   @Override
-  public ParseTree visitParam(AgParser.ParamContext ctx) {
-    return build(ctx, AgParser.ParamContext::new);
+  public ParseTree visitParam(ParamContext ctx) {
+    return build(ctx, ParamContext::new);
   }
 
   @Override
-  public ParseTree visitFragmentRef(AgParser.FragmentRefContext ctx) {
-    return build(ctx, AgParser.FragmentRefContext::new);
+  public ParseTree visitFragmentRef(FragmentRefContext ctx) {
+    return build(ctx, FragmentRefContext::new);
   }
 
   @Override
-  public ParseTree visitTypeRef(AgParser.TypeRefContext ctx) {
-    return build(ctx, AgParser.TypeRefContext::new);
+  public ParseTree visitTypeRef(TypeRefContext ctx) {
+    return build(ctx, TypeRefContext::new);
   }
 
   @Override
-  public ParseTree visitTypeArgs(AgParser.TypeArgsContext ctx) {
-    return build(ctx, AgParser.TypeArgsContext::new);
+  public ParseTree visitTypeArgs(TypeArgsContext ctx) {
+    return build(ctx, TypeArgsContext::new);
   }
 
   @Override
-  public ParseTree visitTypeArg(AgParser.TypeArgContext ctx) {
-    return build(ctx, AgParser.TypeArgContext::new);
+  public ParseTree visitTypeArg(TypeArgContext ctx) {
+    return build(ctx, TypeArgContext::new);
   }
 
   @Override
-  public ParseTree visitWildcard(AgParser.WildcardContext ctx) {
-    return build(ctx, AgParser.WildcardContext::new);
+  public ParseTree visitWildcard(WildcardContext ctx) {
+    return build(ctx, WildcardContext::new);
   }
 
   @Override
-  public ParseTree visitTypeParams(AgParser.TypeParamsContext ctx) {
-    return build(ctx, AgParser.TypeParamsContext::new);
+  public ParseTree visitTypeParams(TypeParamsContext ctx) {
+    return build(ctx, TypeParamsContext::new);
   }
 
   @Override
-  public ParseTree visitTypeParam(AgParser.TypeParamContext ctx) {
-    return build(ctx, AgParser.TypeParamContext::new);
+  public ParseTree visitTypeParam(TypeParamContext ctx) {
+    return build(ctx, TypeParamContext::new);
   }
 
   @Override
-  public ParseTree visitBounds(AgParser.BoundsContext ctx) {
-    return build(ctx, AgParser.BoundsContext::new);
+  public ParseTree visitBounds(BoundsContext ctx) {
+    return build(ctx, BoundsContext::new);
   }
 
   @Override
-  public ParseTree visitName(AgParser.NameContext ctx) {
-    return build(ctx, AgParser.NameContext::new);
+  public ParseTree visitName(NameContext ctx) {
+    return build(ctx, NameContext::new);
   }
 
   @Override
-  public ParseTree visitQualifier(AgParser.QualifierContext ctx) {
-    return build(ctx, AgParser.QualifierContext::new);
+  public ParseTree visitQualifier(QualifierContext ctx) {
+    return build(ctx, QualifierContext::new);
   }
 
   @Override
