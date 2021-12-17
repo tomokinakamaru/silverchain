d41cd52ad1226fe09e63b73da48c51962a76ce14
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2021-12-17 10:16:26 +0900

    Refactor FragmentResolver

--- src/main/java/silverchain/internal/frontend/rewriter/FragmentResolver.java
+++ src/main/java/silverchain/internal/frontend/rewriter/FragmentResolver.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.apiguardian.api.API;
-import silverchain.internal.frontend.antlr.AgParser.ChainElemContext;
 import silverchain.internal.frontend.antlr.AgParser.ChainExprContext;
 import silverchain.internal.frontend.antlr.AgParser.FragmentDeclContext;
 import silverchain.internal.frontend.antlr.AgParser.FragmentRefContext;
@@ -24,16 +23,9 @@ public class FragmentResolver extends ContextRewriter {
   }
 
   @Override
-  public ParseTree visitChainElem(ChainElemContext ctx) {
-    ctx = (ChainElemContext) ctx.accept(new ContextReplicator());
-    FragmentRefContext r = ctx.fragmentRef();
-    if (r != null) {
-      ChainExprContext refCtx = fragments.get(r.FRAGMENT_ID().getText());
-      ChainExprContext newCtx = (ChainExprContext) refCtx.accept(new ContextReplicator());
-      ChainElemContext elem = (ChainElemContext) ctx.accept(new ContextReplicator());
-      elem.children.replaceAll(t -> t == elem.fragmentRef() ? newCtx : t);
-      return elem;
-    }
-    return ctx;
+  public ParseTree visitFragmentRef(FragmentRefContext ctx) {
+    ctx = (FragmentRefContext) super.visitFragmentRef(ctx);
+    ChainExprContext refCtx = fragments.get(ctx.FRAGMENT_ID().getText());
+    return refCtx.accept(new ContextReplicator());
   }
 }
